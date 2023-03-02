5f9571751a59ee313faaa070d92d6195b41c9061
Loxico <paolocuzcano35@gmail.com>
2023-03-02 13:28:20 -0500

    support for multiple subdirectories

--- src/client/registry.js
+++ src/client/registry.js
@@ -1,37 +1,39 @@
 const fs = require("fs");
 const path = require("path");
 
-const filter = (file) => file.endsWith(".js");
 const root = path.join(process.cwd(), "src");
 
+function getFiles(dir) {
+  const filePath = path.join(root, dir);
+  return fs.readdirSync(filePath).flatMap((file) => {
+    const stat = fs.lstatSync(path.join(filePath, file));
+    if (stat.isDirectory()) return getFiles(path.join(dir, file));
+    if (!file.endsWith(".js")) return [];
+    return path.join(filePath, file);
+  });
+}
+
 function registryCommands(client) {
-  const folders = fs.readdirSync(path.join(root, "commands"));
-  for (const folder of folders) {
-    const files = fs.readdirSync(path.join(root, "commands", folder)).filter(filter);
-    for (const file of files) {
-      const Command = require(path.join(root, "commands", folder, file));
-      const command = new Command(client);
-      client.commands.set(command.name, command);
-    }
+  const files = getFiles("commands");
+  for (const file of files) {
+    const Command = require(path.join(file));
+    const command = new Command(client);
+    client.commands.set(command.name, command);
   }
 
-  if (client.commands.size === 0) {
-    console.info("[Commands (/)] - 0");
-    return;
-  }
-  console.log("[Commands] Loaded");
+  if (client.commands.size === 0) console.info("[Commands (/)] - 0");
+  else console.log("[Commands] Loaded");
 }
 
 function registryEvents(client) {
-  const folders = fs.readdirSync(path.join(root, "events"));
-  for (const folder of folders) {
-    const files = fs.readdirSync(path.join(root, "events", folder)).filter(filter);
-    for (const file of files) {
-      const Event = require(path.join(root, "events", folder, file));
-      const event = new Event(client);
-      client[event.once ? "once" : "on"](event.name, (...args) => event.run(...args));
-    }
+  const files = getFiles("events");
+  for (const file of files) {
+    const Event = require(file);
+    const event = new Event(client);
+    const onOrOnce = event.once ? "once" : "on";
+    client[onOrOnce](event.name, (...args) => event.run(...args));
   }
+
   console.log("[Events] Loaded");
 }
 

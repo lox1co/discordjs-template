fb3280ff247d88b50fa3a832f067520565067d22
Loxico <paolocuzcano35@gmail.com>
2023-03-02 13:37:04 -0500

    support for multiple subdirectories

--- src/client/registry.ts
+++ src/client/registry.ts
@@ -5,39 +5,40 @@ import type Client from ".";
 import { Command as BaseCommand, Event as BaseEvent } from ".";
 
 const envDev = process.env.NODE_ENV === "develompent";
-const filter = (file: string): boolean => file.endsWith(envDev ? ".ts" : ".js");
 const root = path.join(process.cwd(), envDev ? "src" : "dist");
 
+function getFiles(dir: string): string[] {
+  const filePath = path.join(root, dir);
+  return fs.readdirSync(filePath).flatMap((file) => {
+    const stat = fs.lstatSync(path.join(filePath, file));
+    if (stat.isDirectory()) return getFiles(path.join(dir, file));
+    if (!file.endsWith(envDev ? ".ts" : ".js")) return [];
+    return path.join(filePath, file);
+  });
+}
+
 export async function registryCommands(client: Client): Promise<void> {
-  const folders = fs.readdirSync(path.join(root, "commands"));
-  for (const folder of folders) {
-    const files = fs.readdirSync(path.join(root, "commands", folder)).filter(filter);
-    for (const file of files) {
-      const { default: Slash } = await import(path.join(root, "commands", folder, file));
-      if (Slash.prototype instanceof BaseCommand) {
-        const slash: BaseCommand = new Slash(client);
-        client.commands.set(slash.name, slash);
-      }
+  const files = getFiles("commands");
+  for (const file of files) {
+    const { default: Slash } = await import(file);
+    if (Slash.prototype instanceof BaseCommand) {
+      const slash: BaseCommand = new Slash(client);
+      client.commands.set(slash.name, slash);
     }
   }
 
-  if (client.commands.size === 0) {
-    console.info("[Commands (/)] - 0");
-    return;
-  }
-  console.log("[Commands] Loaded");
+  if (client.commands.size === 0) console.info("[Commands (/)] - 0");
+  else console.log("[Commands] Loaded");
 }
 
 export async function registryEvents(client: Client): Promise<void> {
-  const folders = fs.readdirSync(path.join(root, "events"));
-  for (const folder of folders) {
-    const files = fs.readdirSync(path.join(root, "events", folder)).filter(filter);
-    for (const file of files) {
-      const { default: Event } = await import(path.join(root, "events", folder, file));
-      if (Event.prototype instanceof BaseEvent) {
-        const event: BaseEvent = new Event(client);
-        client[event.once ? "once" : "on"](event.name, (...args) => event.run(...args));
-      }
+  const files = getFiles("events");
+  for (const file of files) {
+    const { default: Event } = await import(file);
+    if (Event.prototype instanceof BaseEvent) {
+      const event: BaseEvent = new Event(client);
+      const onOrOnce = event.once ? "once" : "on";
+      client[onOrOnce](event.name, (...args) => event.run(...args));
     }
   }
   console.log("[Events] Loaded");
